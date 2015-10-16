1b1d14b8bf1ae101dee60f8f63dd260174d81494
dividesbyzer0 <patrickolila.15104011.ilc@gmail.com>
2015-10-16 10:03:39 -0400

    Create helloworld.rb

---
+++ bin/helloworld.rb
@@ -0,0 +1,9 @@
+print 'Hello World!' + "\n" # Outputs string 'Hello World!'
+print 'What is your name?' # Outputs string 'What is your name?'
+name = gets.chomp # Sets the variable name by getting input
+print 'How old are you?'
+age = gets.chomp
+print 'In what city do you live?'
+location = gets.chomp
+print 'Hello ' + name + '! You are ' + age + ' and live in ' + location + '.'
+sleep 5
